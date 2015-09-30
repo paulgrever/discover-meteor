ff8467d633ed35285a01c32c4a736ddc4459d801
Paul Grever <paulgrever@gmail.com>
2015-09-29 22:18:02 -0600

    finished chapter 7

---
+++ server/fixtures.js
@@ -0,0 +1,22 @@
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  });
+
+  Posts.insert({
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  });
+
+  Posts.insert({
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  });
+
+  Posts.insert({
+    title: 'Meteor Docs',
+    author: 'Tom Coleman',
+    url: 'http://docs.meteor.com'
+  });
+}
